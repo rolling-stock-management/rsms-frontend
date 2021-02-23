f03543c54ddc1deeef1f2cd240f0c230a1fe6d52
Stoyan Haydushki <s.haidushky@abv.bg>
2021-02-23 17:43:10 +0200

    Add sidebar, navbar, dashboard layout
    Added a test page for dashboard index.

---
+++ components/navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-navbar
+    toggleable="lg"
+    type="light"
+    variant="white"
+    class="shadow-sm sticky-top"
+  >
+    <b-navbar-brand href="#">СУПЖПС</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item to="/" exact exact-active-class="active">Начало</b-nav-item>
+        <b-nav-item v-if="guest" to="login" exact exact-active-class="active"
+          >Вход</b-nav-item
+        >
+        <b-nav-item v-if="guest" to="register" exact exact-active-class="active"
+          >Регистрация</b-nav-item
+        >
+        <b-nav-item v-if="loggedIn" to="login" exact exact-active-class="active"
+          >Табло за управление</b-nav-item
+        >
+        <b-nav-item v-if="loggedIn" @click="logOut"
+          ><b-icon-box-arrow-right></b-icon-box-arrow-right
+        ></b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  methods: {
+    async logOut() {
+      // TODO: Alert for testing purposes
+      alert('logging out')
+      await this.$auth.logout()
+    },
+  },
+  computed: {
+    guest() {
+      return !this.$auth.loggedIn
+    },
+    loggedIn() {
+      return this.$auth.loggedIn
+    },
+  },
+}
+</script>
---
+++ components/sidebar.vue
@@ -0,0 +1,96 @@
+<template>
+  <b-collapse
+    id="nav-collapse"
+    class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+  >
+    <div class="sidebar-sticky pt-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <b-link
+            to="dashboard"
+            class="nav-link"
+            exact
+            exact-active-class="active"
+            >Dashboard</b-link
+          >
+        </li>
+        <li class="nav-item">
+          <b-link to="test" class="nav-link" exact exact-active-class="active"
+            >Test</b-link
+          >
+        </li>
+      </ul>
+
+      <h6
+        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted"
+      >
+        <span>Saved reports</span>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <b-link to="test" class="nav-link" exact exact-active-class="active"
+            >Test</b-link
+          >
+        </li>
+      </ul>
+    </div>
+  </b-collapse>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+/*
+ * Sidebar
+ */
+
+.sidebar {
+  position: s;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, 0.1);
+}
+
+.sidebar-sticky {
+  position: relative;
+  top: 0;
+  height: calc(100vh - 48px);
+  padding-top: 0.5rem;
+  overflow-x: hidden;
+  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+}
+
+@supports ((position: -webkit-sticky) or (position: sticky)) {
+  .sidebar-sticky {
+    position: -webkit-sticky;
+    position: sticky;
+  }
+}
+
+.sidebar .nav-link {
+  font-weight: 500;
+  color: #333;
+}
+
+.sidebar .nav-link .feather {
+  margin-right: 4px;
+  color: #999;
+}
+
+.sidebar .nav-link.active {
+  color: #007bff;
+}
+
+.sidebar .nav-link:hover .feather,
+.sidebar .nav-link.active .feather {
+  color: inherit;
+}
+
+.sidebar-heading {
+  font-size: 0.75rem;
+  text-transform: uppercase;
+}
+</style>
