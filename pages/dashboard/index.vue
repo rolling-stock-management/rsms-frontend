5ad59ded54ec1dc5c0ec3380a024913e66bce973
Stoyan Haydushki <s.haidushky@abv.bg>
2021-03-04 22:44:40 +0200

    Update dashboard index
    Add role and permissions check;
    Add links.

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -6,19 +6,39 @@
     <b-card-text>
       Добре дошли! Навигирайте из сайта чрез страничната лента за избор.
     </b-card-text>
-    <b-button variant="outline-primary" to="/dashboard/" class="my-1"
+    <b-button
+      variant="outline-primary"
+      to="/dashboard/"
+      class="my-1"
+      v-if="userHasRole('passenger-reporter')"
       >Извърши проверка на вагон</b-button
     >
-    <b-button variant="outline-primary" to="/dashboard/" class="my-1"
+    <b-button
+      variant="outline-primary"
+      to="/dashboard/"
+      class="my-1"
+      v-if="userHasPermission('passenger-wagon-train-create')"
       >Маркирай вагони на влак</b-button
     >
-    <b-button variant="outline-primary" to="/dashboard/" class="my-1"
+    <b-button
+      variant="outline-primary"
+      to="/dashboard/passenger-wagons"
+      class="my-1"
+      v-if="userHasRole('passenger-manager')"
       >Пътнически вагони</b-button
     >
-    <b-button variant="outline-primary" to="/dashboard/" class="my-1"
+    <b-button
+      variant="outline-primary"
+      to="/dashboard/freight-wagons"
+      class="my-1"
+      v-if="userHasRole('freight-manager')"
       >Товарни вагони</b-button
     >
-    <b-button variant="outline-primary" to="/dashboard/" class="my-1"
+    <b-button
+      variant="outline-primary"
+      to="/dashboard/tractive-units"
+      class="my-1"
+      v-if="userHasRole('locmotive-manager')"
       >Локомотиви</b-button
     >
   </b-card>
@@ -27,5 +47,13 @@
 <script>
 export default {
   layout: 'dashboard',
+  methods: {
+    userHasPermission(permission) {
+      return this.$UserRolePermissionPlugin.userHasPermission(permission)
+    },
+    userHasRole(role) {
+      return this.$UserRolePermissionPlugin.userHasRole(role)
+    },
+  },
 }
 </script>
